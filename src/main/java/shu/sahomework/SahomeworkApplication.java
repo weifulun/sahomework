8f635c32dd63d6aaea98075845c09d48243bb8fe
weifulun <641167974@qq.com>
2019-11-28 14:09:30 +0800

    初始化项目，其中application.properties包含数据库连接的账号密码

---
+++ src/main/java/shu/sahomework/SahomeworkApplication.java
@@ -0,0 +1,13 @@
+package shu.sahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SahomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shu/sahomework/SahomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package shu.sahomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
